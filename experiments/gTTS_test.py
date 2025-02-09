9375783d784ee432dd3344eeb1d9d34cd4636e5e
Ebad Qureshi <ebad.qureshi922@gmail.com>
2025-02-09 03:03:16 -0500

    SWEN5239-9: gTTS sample program implemented

--- experiments/gTTS_test.py
+++ experiments/gTTS_test.py
@@ -1,1 +1,27 @@
+import os
+import pygame
 from gtts import gTTS
+
+
+# generate speech
+tts = gTTS(text='Hello world!', lang='en')
+
+# save audio in temporary file
+temp_filename = 'audio.mp3'
+tts.save(temp_filename)
+
+# initialize pygame mixer to play back audio
+pygame.init()
+pygame.mixer.init()
+
+# load and play the audio
+pygame.mixer.music.load(temp_filename)
+pygame.mixer.music.play()
+
+# wait until the audio finishes playing
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+# clean up and remove the temp file
+pygame.mixer.quit()
+os.remove(temp_filename)
