61c453e2d74b4a689e6a349881891370e32bbb46
Ebad Qureshi <ebad.qureshi922@gmail.com>
2025-02-24 22:28:56 -0500

    SWEN5239-20: fixed button widths on main menu

--- src/main_menu.py
+++ src/main_menu.py
@@ -24,22 +24,25 @@ class MainMenu(tk.Frame):
         logo_label = tk.Label(self, image=self.logo)
         logo_label.pack(pady=20)
 
+        # Define button width
+        button_width = 20
+
         # Knowledge Test Button
-        button = ttk.Button(self, text="Knowledge Test", command=navigate_to_coming_soon)
+        button = ttk.Button(self, text="Knowledge Test", command=navigate_to_coming_soon, width=button_width)
         button.pack(pady=10, ipadx=20, ipady=5)
 
         # Reading Practice Button
-        button = ttk.Button(self, text="Reading Practice", command=navigate_to_coming_soon)
+        button = ttk.Button(self, text="Reading Practice", command=navigate_to_coming_soon, width=button_width)
         button.pack(pady=10, ipadx=20, ipady=5)
 
         # Listening Exercise Button
-        button = ttk.Button(self, text="Listening Exercise", command=navigate_to_listening_exercise)
+        button = ttk.Button(self, text="Listening Exercise", command=navigate_to_listening_exercise, width=button_width)
         button.pack(pady=10, ipadx=20, ipady=5)
 
         # Speaking Practice Button
-        button = ttk.Button(self, text="Speaking Practice", command=navigate_to_speaking_practice)
+        button = ttk.Button(self, text="Speaking Practice", command=navigate_to_speaking_practice, width=button_width)
         button.pack(pady=10, ipadx=20, ipady=5)
 
         # Exit Button
-        exit_button = ttk.Button(self, text="Exit", command=parent.quit)
+        exit_button = ttk.Button(self, text="Exit", command=parent.quit, width=button_width)
         exit_button.pack(pady=10, ipadx=20, ipady=5)
